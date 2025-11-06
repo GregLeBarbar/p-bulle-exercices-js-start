11452114185c8dbe86920e459248fd41cb5fec06
Grégory Charmier <px86dym@etmlnet.local>
2025-11-06 13:12:12 +0100

    feat(hello-world)

---
+++ exercices/hello-world/hello-world.js
@@ -0,0 +1,8 @@
+//
+// Ceci est uniquement le SQUELETTE de l’exercice « Hello World ».
+// Il t’est fourni pour te permettre de démarrer plus rapidement l’écriture du code.
+//
+
+export function hello() {
+  throw new Error("Remove this line and implement the function");
+}
---
+++ exercices/hello-world/hello-world.spec.js
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "@jest/globals";
+import { hello } from "./hello-world";
+describe("Hello World", () => {
+  test("Say Hi!", () => {
+    expect(hello()).toEqual("Hello, World!");
+  });
+});
