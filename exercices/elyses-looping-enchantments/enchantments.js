a8033c409b81b979c6275d60a37e690b376edcdb
neo-darbellay <178883466+neo-darbellay@users.noreply.github.com>
2025-11-21 14:45:28 +0100

    feat(elyses-looping-enchantments\enchantments.js): Terminer l'exercice
    [13] [Done]

--- exercices/elyses-looping-enchantments/enchantments.js
+++ exercices/elyses-looping-enchantments/enchantments.js
@@ -9,8 +9,14 @@
  * @returns {number} number of cards of a single type there are in the deck
  */
 export function cardTypeCheck(stack, card) {
-  // 🚨 Use .forEach
-  throw new Error('Implement the cardTypeCheck function');
+  let numberOfCards = 0;
+
+  stack.forEach((individualCard) => {
+    if (card == individualCard)
+      numberOfCards++;
+  });
+
+  return numberOfCards;
 }
 
 /**
@@ -21,6 +27,20 @@ export function cardTypeCheck(stack, card) {
  * @returns {number} number of cards that are either odd or even (depending on `type`)
  */
 export function determineOddEvenCards(stack, type) {
-  // 🚨 Use a `for...of` loop
-  throw new Error('Implement the determineOddEvenCards function');
+  let numberOfOddEvenCards = 0;
+
+  for (const card of stack) {
+    if (type)
+    {
+      if ((card % 2 == 1)) {
+        continue;
+      }
+    } else if (card % 2 == 0) {
+      continue;
+    }
+
+      numberOfOddEvenCards++;
+  }
+
+  return numberOfOddEvenCards;
 }
