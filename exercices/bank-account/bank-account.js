574803a80c65282e66a68639bcefb48c744a5941
neo-darbellay <178883466+neo-darbellay@users.noreply.github.com>
2025-12-05 13:22:05 +0100

    feat(bank-account.js): Terminer l'exercice
    [15] [Done]

--- exercices/bank-account/bank-account.js
+++ exercices/bank-account/bank-account.js
@@ -4,33 +4,63 @@
 //
 
 export class BankAccount {
+  #accountStatus; // # = private
+  #accountBalance;
+
   constructor() {
-    throw new Error('Remove this line and implement the function');
+    this.#accountBalance = 0;
+    this.#accountStatus = "Not open";
   }
 
   open() {
-    throw new Error('Remove this line and implement the function');
+    if (this.#accountStatus != "Open") {
+      this.#accountStatus = "Open";
+      this.#accountBalance = 0;
+    } else {
+      throw new ValueError("Account is already open.");
+    }
   }
 
   close() {
-    throw new Error('Remove this line and implement the function');
+    if (this.#accountStatus == "Open") {
+      this.#accountStatus = "Closed";
+    } else {
+      throw new ValueError("Account not open.");
+    }
   }
 
-  deposit() {
-    throw new Error('Remove this line and implement the function');
+  deposit(number) {
+    if (this.#accountStatus == "Open" && number > 0) {
+      this.#accountBalance += number;
+    } else {
+      throw new ValueError("Account not open.");
+    }
   }
 
-  withdraw() {
-    throw new Error('Remove this line and implement the function');
+  withdraw(number) {
+    if (this.#accountStatus == "Open") {
+      if ((this.#accountBalance - number) >= 0 && number > 0) {
+        this.#accountBalance -= number;
+      } else {
+        throw new ValueError("Cannot withdraw negative.");
+      }
+    } else {
+      throw new ValueError("Account not open.");
+    }
   }
 
   get balance() {
-    throw new Error('Remove this line and implement the function');
+    if (this.#accountStatus == "Open") {
+      return this.#accountBalance;
+    } else {
+      throw new ValueError("Account not open.");
+    }
+    2;
   }
 }
 
 export class ValueError extends Error {
   constructor() {
-    super('Bank account error');
+    super("Bank account error");
   }
 }
