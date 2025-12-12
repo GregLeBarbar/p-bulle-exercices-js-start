2f4075a031034bcee2c7661d33b475d4da1b07f2
neo-darbellay <178883466+neo-darbellay@users.noreply.github.com>
2025-12-12 15:43:14 +0100

    feat(book-store.js): Essais
    [120] [Not Done]

--- exercices/book-store/book-store.js
+++ exercices/book-store/book-store.js
@@ -13,30 +13,84 @@ export const cost = (books) => {
     booksAmount[book - 1] += 1;
   });
 
-  while (booksAmount.some((amount) => amount > 0)) {
-    booksAmount.sort();
+  function compareNumbers(a, b) {
+    return a - b;
+  }
 
-    let currentDiscount = 1;
-    let bookAmount = 1;
-    if (
-      booksAmount[0] > 1 &&
-      booksAmount[1] > 1 &&
-      booksAmount[2] > 1 &&
-      booksAmount[3]
-    ) {
+  function canRemoveFromNumber(number, remove) {
+    return number - remove >= 0 ? true : false;
+  }
+
+  function findHighestGroup(groupsOf) {
+    let groupAmount = 0;
+
+    let booksToCheck = [].concat(booksAmount);
+
+    let highest = 0;
+    for (let i = 0; i < booksToCheck.length; i++) {
+      if (booksToCheck[i] > highest) {
+        highest = booksToCheck[i];
+      }
+    }
+
+    let i = 0;
+    let canContinue = true;
+
+    while (canContinue) {
+      booksToCheck.sort(compareNumbers);
+      booksToCheck.reverse();
+      booksToCheck = booksToCheck
+        .filter((a) => a > 0)
+        .concat(booksToCheck.filter((a) => a == 0));
+
+      canContinue = booksToCheck.every((amount, index) =>
+        index < groupsOf ? canRemoveFromNumber(amount, 1) : true
+      );
+
+      if (
+        canContinue &&
+        i > groupAmount &&
+        booksToCheck.every((amount, index) =>
+          index < groupsOf ? canRemoveFromNumber(amount, 1) : true
+        )
+      ) {
+        for (let bookIndex = 0; bookIndex < booksToCheck.length; bookIndex++) {
+          booksToCheck[bookIndex]--;
+        }
+
+        groupAmount++;
+      }
+
+      i++;
+    }
+
+    return groupAmount;
+  }
+
+  while (booksAmount.some((amount) => amount > 0)) {
+    booksAmount.sort(compareNumbers);
+    booksAmount.reverse();
+    booksAmount = booksAmount
+      .filter((a) => a > 0)
+      .concat(booksAmount.filter((a) => a == 0));
+
+    let currentDiscount = 0;
+    let bookAmount = 0;
+
+    const groupsOf5 = findHighestGroup(5);
+    const groupsOf4 = findHighestGroup(4);
+    const groupsOf3 = findHighestGroup(3);
+    const groupsOf2 = findHighestGroup(2);
+    const groupsOf1 = findHighestGroup(1);
+
+    if (groupsOf4 >= 1 && !(groupsOf5 == 1 && groupsOf3 == 1)) {
       bookAmount = 4;
       currentDiscount = 0.8;
       booksAmount[0]--;
       booksAmount[1]--;
       booksAmount[2]--;
       booksAmount[3]--;
-    } else if (
-      booksAmount[0] > 1 &&
-      booksAmount[1] > 1 &&
-      booksAmount[2] > 1 &&
-      booksAmount[3] > 1 &&
-      booksAmount[4] > 1
-    ) {
+    } else if (groupsOf5 >= 1) {
       bookAmount = 5;
       currentDiscount = 0.75;
       booksAmount[0]--;
@@ -44,25 +98,27 @@ export const cost = (books) => {
       booksAmount[2]--;
       booksAmount[3]--;
       booksAmount[4]--;
-    } else if (booksAmount[0] > 1 && booksAmount[1] > 1) {
-      bookAmount = 2;
-      currentDiscount = 0.95;
-      booksAmount[0]--;
-      booksAmount[1]--;
-    } else if (booksAmount[0] > 1 && booksAmount[1] > 1 && booksAmount[2] > 1) {
+    } else if (groupsOf3 >= 1) {
       bookAmount = 3;
       currentDiscount = 0.9;
       booksAmount[0]--;
       booksAmount[1]--;
       booksAmount[2]--;
+    } else if (groupsOf2 >= 1) {
+      bookAmount = 2;
+      currentDiscount = 0.95;
+      booksAmount[0]--;
+      booksAmount[1]--;
     } else {
+      bookAmount = 1;
+      currentDiscount = 1;
       booksAmount[0]--;
     }
 
     totalCost += bookAmount * baseCost * currentDiscount;
   }
 
-  // This hurt my brain
+  // This still hurts my brain
 
   return totalCost;
 };
