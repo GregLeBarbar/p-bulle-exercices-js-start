918a9a3dcd9afff7c2297e7773852d85bf1b23aa
neo-darbellay <178883466+neo-darbellay@users.noreply.github.com>
2026-01-09 13:24:46 +0100

    feat(rarity): Created the main javascript file
    [10] [Done]

---
+++ exercices/rarity/rarity.js
@@ -0,0 +1,17 @@
+//
+// This is only a SKELETON file for the 'Simple Cipher' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+/**
+ * Picks a random item based on its weight.
+ *
+ * The higher the weight value is, the more often it will be picked (probability)
+ *
+ * @param {any[]} items Any items
+ * @param {number[]} weight Their weight
+ * @returns {any} The chosen item
+ */
+export function weightedRandom(items, weight) {
+  throw new Error('Remove this line and implement the function');
+}
